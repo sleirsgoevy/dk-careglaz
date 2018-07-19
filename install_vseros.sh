5e300a243068360d7bc5d6988c881755def5138c
sleirsgoevy <sleirsgoevy@gmail.com>
2018-07-19 22:31:46 +0300

    Make install_vseros.sh more user-friendly

--- install_vseros.sh
+++ install_vseros.sh
@@ -1,7 +1,15 @@
-mkdir tasksheets/$1
-mkdir scoreboard/$1
-echo "$2" > tasksheets/$1/name.txt
-echo $3 > tasksheets/$1/sortid.txt
+if [ "x$4" == x ]
+then
+    cat >&2 << EOF
+Usage: install_vseros.sh <id> <name> <sortid> <archive_dir>
+EOF
+    exit 1
+fi
+
+mkdir data/tasksheets/$1
+mkdir data/scoreboard/$1
+echo "$2" > data/tasksheets/$1/name.txt
+echo $3 > data/tasksheets/$1/sortid.txt
 echo "import vseros
-tasks = vseros.prepare_contest(\"$4\")" > tasksheets/$1/tester.py
+tasks = vseros.prepare_contest(\"$4\")" > data/tasksheets/$1/tester.py
 
