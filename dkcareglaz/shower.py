4fef50ca7e5042b7d0cd931e28ba2ec8645dc02a
sleirsgoevy <sleirsgoevy@gmail.com>
2018-12-17 21:03:24 +0300

    Fix error 404 on theory and tasks.

--- dkcareglaz/shower.py
+++ dkcareglaz/shower.py
@@ -3,11 +3,11 @@ from bottle import static_file, response
 from os.path import isfile, sep as pathsep, split as pathsplit
 from . import __path__ as pkg_path
 
-def file(path, error=200, kick=False):
+def file(path, error=200, kick=False, root=pathsplit(pkg_path[0])[0]):
     if kick:
         response.set_cookie('credentials', 'invalid', path="/")
     response.status = error
-    return static_file(path, pathsplit(pkg_path[0])[0])
+    return static_file(path, root)
 
 @the_app.route('/theory/<name>')
 def theory(name):
@@ -16,7 +16,7 @@ def theory(name):
 #       response.status = 403
 #       return '<html><head><title>Теория</title></head><body><h3>'\
 #              'Замечание для умных</h3><p>Не пытайтесь меня крякнуть!</p></body></html>'
-    return file('tasksheets/'+name+'/theory.html')
+    return file('tasksheets/'+name+'/theory.html', root='.')
 
 @the_app.route('/tasks/<name>')
 def tasks(name):
@@ -25,4 +25,4 @@ def tasks(name):
 #       response.status = 403
 #       return '<html><head><title>Задачи</title></head><body><h3>'\
 #              'Замечание для умных</h3><p>Не пытайтесь меня крякнуть!</p></body></html>'
-    return file('tasksheets/'+name+'/tasks.html')
+    return file('tasksheets/'+name+'/tasks.html', root='.')
